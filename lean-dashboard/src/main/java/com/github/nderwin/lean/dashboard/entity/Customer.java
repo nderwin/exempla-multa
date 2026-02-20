edde0d1b0a32fcbbd54b4dce574f31cc2a91b599
Nathan Erwin <nathan.d.erwin@gmail.com>
2026-02-19 19:35:02 -0500

    * investigating simple server side semantic HTML for a dashboard
    Signed-off-by:Nathan Erwin <nathan.d.erwin@gmail.com>

---
+++ lean-dashboard/src/main/java/com/github/nderwin/lean/dashboard/entity/Customer.java
@@ -0,0 +1,10 @@
+package com.github.nderwin.lean.dashboard.entity;
+
+public record Customer(
+        String id,
+        String name,
+        String email,
+        String status,
+        String recentNote) {
+
+}
