9fe65bc9157382144f83d83c95dab33534e42fe9
Nathan Erwin <nathan.d.erwin@gmail.com>
2026-02-24 21:00:49 -0500

    REST Links
    * investigating the rest-links extension for adding HATEOAS style links
    
    Signed-off-by:Nathan Erwin <nathan.d.erwin@gmail.com>

--- product-catalog/src/main/java/com/github/nderwin/scrolling/boundary/ProductDTO.java
+++ product-catalog/src/main/java/com/github/nderwin/scrolling/boundary/ProductDTO.java
@@ -1,8 +1,10 @@
 package com.github.nderwin.scrolling.boundary;
 
+import io.quarkus.resteasy.reactive.links.RestLinkId;
 import java.math.BigDecimal;
 
 public record ProductDTO(
+        @RestLinkId
         Long id,
         String name,
         String category,
