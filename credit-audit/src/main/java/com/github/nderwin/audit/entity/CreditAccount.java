3ea2ef97f3da8f5f466098f306d6ceaa5692f82f
Nathan Erwin <nathan.d.erwin@gmail.com>
2026-02-25 21:55:47 -0500

    Auditing for collections
    * investigating auditing for element collections
    
    Signed-off-by:Nathan Erwin <nathan.d.erwin@gmail.com>

--- credit-audit/src/main/java/com/github/nderwin/audit/entity/CreditAccount.java
+++ credit-audit/src/main/java/com/github/nderwin/audit/entity/CreditAccount.java
@@ -1,9 +1,13 @@
 package com.github.nderwin.audit.entity;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import org.hibernate.envers.Audited;
 
 import static com.github.nderwin.audit.entity.CreditAccount.Status.ACTIVE;
@@ -19,6 +23,11 @@ public class CreditAccount extends PanacheEntity {
     
     @Enumerated(EnumType.STRING)
     private Status status = ACTIVE;
+    
+    @ElementCollection
+    List<AccountHolder> accountHolders = new ArrayList<>();
+    
+    // TODO - other types of collections
 
     protected CreditAccount() {
     }
@@ -46,6 +55,18 @@ public class CreditAccount extends PanacheEntity {
     
     public void suspend() {
         this.status = SUSPENDED;
+    }
+
+    public List<AccountHolder> getAccountHolders() {
+        return Collections.unmodifiableList(accountHolders);
+    }
+    
+    public boolean addAccountHolder(final AccountHolder holder) {
+        if (null == holder) {
+            throw new IllegalArgumentException("Account holder must not be null");
+        }
+        
+        return accountHolders.add(holder);
     }
     
     public enum Status {
